e28c169d6a9f83ffc2497945ba00a26761b6f3d7
erdaulet6 <155520960+erdaulet6@users.noreply.github.com>
2024-01-31 23:32:48 +0600

    Initial commit

---
+++ src/MaximumSelectionMethod.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MaximumSelectionMethod {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            int maxIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[maxIndex];
+            arr[maxIndex] = temp;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        scanner.close();
+    }
+}
